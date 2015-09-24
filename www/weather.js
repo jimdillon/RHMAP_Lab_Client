5299c7e00a5734437760ddb9d2a8b3991c3e8341
Jim Dillon <jdillon@jdillon.remote.csb>
2015-09-23 22:34:28 -0400

    final

--- www/weather.js
+++ www/weather.js
@@ -1,19 +1,25 @@
 document.getElementById('get_temp').onclick = function () {
-  document.getElementById('cloudResponse').innerHTML = "<p>Calling Cloud.....</p>";
+  var city = document.getElementById('city').value;
+  var state = document.getElementById('state').value;
 
-  var path = '/' + document.getElementById('city').value
-  + '/' + document.getElementById('state').value;
+  if(city.length < 1  || state.length < 1) {
+    alert('Please enter City and State.');
+  }
+  else {
+    document.getElementById('cloudResponse').innerHTML = '<p>Calling Cloud.....</p>';
+    var path = '/weather/temperature/' + city + '/' + state;
 
-  $fh.cloud(
-      {
-        path: path,
-        method: 'GET'
-      },
-      function (res) {
-        document.getElementById('cloudResponse').innerHTML = "<p>" + res.msg + "</p>";
-      },
-      function (msg, err) {
-        alert('An error occured: ' + msg + ' : ' + JSON.stringify(err));
-      }
-  );
-};
+    $fh.cloud({
+          path: path,
+          method: 'GET'
+        },
+        function (res) {
+          document.getElementById('cloudResponse').innerHTML = '<p>Current '
+          + 'Temperature in ' + city + ', ' + state + ' is ' + res.temperature + '&deg; F</p>';
+        },
+        function (msg, err) {
+          alert('An error occured: ' + JSON.stringify(msg) + ' : ' + JSON.stringify(err));
+        }
+      );
+    }
+  };
